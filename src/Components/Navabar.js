4efd808abfc7a3a15950533b6d5bbc41af87862b
Amit Sali <79369803+amit-sali@users.noreply.github.com>
2021-10-22 18:37:02 +0530

    Update and rename Temporary.js to Navabar.js

---
+++ src/Components/Navabar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../Styles/Temporary.css';
+
+/*Inspiration from this code will be taken for making react components */
+export default function Temporary() {
+    return (<>
+       <header>
+		 <a class="logo" href="/"><img src="logo.svg" alt="logo"></a>
+            <nav>
+                <ul class="nav__links">
+                    <li><a href="#">portfolios</a></li>
+                    <li><a href="#">Live Projects</a></li>
+                    <li><a href="#">About us</a></li>
+                </ul>
+            </nav>
+  
+              <a class="cta" href="#">Log in</a>
+            <p class="menu cta">Menu</p>
+
+	</header>
+
+    </>
+    )
+}
